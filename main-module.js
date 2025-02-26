c165d8fa008a0a9fe1bd3c97318b3338a13ae488
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2025-02-26 18:00:27 +0800

    Revert "Add system instruction and chat history to main module"
    This reverts commit 9dbcac846160ec755e7cc08a1ec03c466fdb1cef.

--- main-module.js
+++ main-module.js
@@ -10,10 +10,8 @@ const API_KEY = gemini_api;
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash" /*"learnlm-1.5-pro-experimental"*/,
-    systemInstruction: 'You are named Jared',
+    //systemInstruction: 'INSERT AN INSTRUCTION HERE',
 });
 
-const chatHistory = [];
 
-
-export {model, chatHistory};
+export {model};
